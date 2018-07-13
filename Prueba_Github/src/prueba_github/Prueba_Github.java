668b01904ec57271284b8ec63b5a4f92811843df
arivas <arivas@192.168.15.6>
2018-07-13 17:49:43 -0500

    Inicialización del proyecto

---
+++ Prueba_Github/src/prueba_github/Prueba_Github.java
@@ -0,0 +1,9 @@
+package prueba_github;
+
+public class Prueba_Github {
+
+    public static void main(String[] args) {
+        System.out.println("Este es el comienzo de la prueba");
+    }
+    
+}
