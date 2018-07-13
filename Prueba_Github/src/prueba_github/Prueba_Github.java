500c11839e5bdfbf42ced14762ab840d0cee78a7
git <git@192.168.15.6>
2018-07-13 18:23:42 -0500

    Se añadió la función para sumar

--- Prueba_Github/src/prueba_github/Prueba_Github.java
+++ Prueba_Github/src/prueba_github/Prueba_Github.java
@@ -2,8 +2,14 @@ package prueba_github;
 
 public class Prueba_Github {
 
-    public static void main(String[] args) {
+    public void main(String[] args) {
         System.out.println("Este es el comienzo de la prueba");
+        System.out.println("La suma de 2 + 2 es: " + Sumar(2,2));
+    }
+    
+    public int Sumar(int _numero1, int _numero2)
+    {
+        return(_numero1 + _numero2);
     }
     
 }
