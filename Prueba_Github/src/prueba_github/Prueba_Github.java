f0c69c2fe85d85cdce3ff81b80fb9614a3f8ebc7
arivas <arivas@192.168.15.6>
2018-07-13 18:37:31 -0500

    Renglón suma

--- Prueba_Github/src/prueba_github/Prueba_Github.java
+++ Prueba_Github/src/prueba_github/Prueba_Github.java
@@ -4,5 +4,6 @@ public class Prueba_Github {
 
     public void main(String[] args) {
         System.out.println("Este es el comienzo de la prueba");
+        System.out.println("La suma de 2+2 es " + 2);
     }
 }
