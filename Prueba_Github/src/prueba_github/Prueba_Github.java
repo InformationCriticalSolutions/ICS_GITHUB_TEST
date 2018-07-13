67305a57f3397973085d06e010d19edd8b05c990
git <git@192.168.15.6>
2018-07-13 18:31:39 -0500

    Agregué la función de multiplicación

--- Prueba_Github/src/prueba_github/Prueba_Github.java
+++ Prueba_Github/src/prueba_github/Prueba_Github.java
@@ -6,6 +6,7 @@ public class Prueba_Github {
         System.out.println("Este es el comienzo de la prueba");
         System.out.println("El resultado de la resta de 2 - 2 es: " + Restar(2,2));
         System.out.println("La suma de 2 + 2 es: " + Sumar(2,2));
+        System.out.println("La multiplicación de 2 * 2 es: " + Multiplicar(2,2));
     }
     
     public int Sumar(int _numero1, int _numero2)
@@ -16,5 +17,10 @@ public class Prueba_Github {
     public int Restar(int _numero1, int _numero2)
     {
         return(_numero1 - _numero2);
+    }
+    
+    public int Multiplicar(int _numero1, int _numero2)
+    {
+        return(_numero1 * _numero2);
     }
 }
