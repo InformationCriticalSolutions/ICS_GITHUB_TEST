9471c872c59433937e2fa19904ec8ff0e80c606c
arivas <arivas@192.168.15.6>
2018-07-13 18:20:13 -0500

    Se añadió el método de resta

--- Prueba_Github/src/prueba_github/Prueba_Github.java
+++ Prueba_Github/src/prueba_github/Prueba_Github.java
@@ -2,8 +2,13 @@ package prueba_github;
 
 public class Prueba_Github {
 
-    public static void main(String[] args) {
+    public void main(String[] args) {
         System.out.println("Este es el comienzo de la prueba");
+        System.out.println("El resultado de la resta de 2 - 2 es: " + Restar(2,2));
     }
     
+    public int Restar(int _numero1, int _numero2)
+    {
+        return(_numero1 - _numero2);
+    }
 }
